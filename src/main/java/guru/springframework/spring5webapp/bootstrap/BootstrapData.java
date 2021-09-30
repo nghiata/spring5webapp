ad0f6b0b2a20d427ba1c7404ceef30841c481f92
nghiata <nghia.ta@asicompany.asia>
2021-09-30 10:35:01 +0700

    Initial commit

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.respositories.AuthorRepository;
+import guru.springframework.spring5webapp.respositories.BookRepository;
+import guru.springframework.spring5webapp.respositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("bootstrap started in here");
+
+        Book book1 = new Book("The lord of the Rings", "123123");
+        Author eric = new Author("Eric", "Gammar");
+        book1.getAuthors().add(eric);
+        eric.getBooks().add(book1);
+        authorRepository.save(eric);
+        bookRepository.save(book1);
+
+        Book book2 = new Book("Harray Porter", "435345362");
+        Author rowling = new Author("JK", "Rowling");
+        book2.getAuthors().add(rowling);
+        rowling.getBooks().add(book2);
+        authorRepository.save(rowling);
+        bookRepository.save(book2);
+
+        Publisher publisher01 = new Publisher("tuoitre", "652 cong hoa");
+        Publisher publisher02 = new Publisher("Thanh nien", "71 Che lan vien");
+        publisherRepository.save(publisher01);
+        publisherRepository.save(publisher02);
+
+        System.out.println("Number of books: "+ bookRepository.count());
+        System.out.println("Number of publishers: "+ publisherRepository.count());
+    }
+}
